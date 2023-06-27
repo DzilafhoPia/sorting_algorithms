0aa9fb24fced0d175a59c3baf97c66ce513d8d12
DzilafhoPia <dzilafhopia17@gmail.com>
2023-06-27 08:15:47 -0700

    sorting algorithms-mandatory

---
+++ 3-quick_sort.c
@@ -0,0 +1,21 @@
+#include "sort.h"
+
+void swap_ints(int *a, int *b);
+int lomuto_partition(int *array, size_t size, int left, int right);
+void lomuto_sort(int *array, size_t size, int left, int right);
+void quick_sort(int *array, size_t size);
+
+/**
+ * swap_ints - Swap two int in an array.
+ * @a: The first int to swap.
+ * @b: The second int to swap.
+ */
+void swap_ints(int *a, int *b)
+{
+	int tmp;
+
+	tmp = *a;
+	*a = *b;
+	*b = tmp;
+}
+
